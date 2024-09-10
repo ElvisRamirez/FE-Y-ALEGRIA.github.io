2a5ba99eea1c5d48ca07cdc69c72f980df4cdfa2
Elvis Ramirez <93403690+ElvisRamirez@users.noreply.github.com>
2024-09-10 08:12:22 -0500

    ASD
    NUEVO

---
+++ escuela/js/preload.js
@@ -0,0 +1,7 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.getElementById('preloader').style.display = 'none';
+        document.querySelector('.contenido').style.display = 'block';
+    }, 1000); // Ajusta este tiempo según lo que consideres adecuado
+});
