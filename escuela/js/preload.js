307b09a6a4c70fd20550f8ae0cecade7d1bc7ab5
Elvis Ramirez <93403690+ElvisRamirez@users.noreply.github.com>
2024-09-12 10:58:56 -0500

    asa

--- escuela/js/preload.js
+++ escuela/js/preload.js
@@ -3,5 +3,5 @@ document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {
         document.getElementById('preloader').style.display = 'none';
         document.querySelector('.contenido').style.display = 'block';
-    }, 1000); // Ajusta este tiempo según lo que consideres adecuado
+    }, 800); // Ajusta este tiempo según lo que consideres adecuado
 });
